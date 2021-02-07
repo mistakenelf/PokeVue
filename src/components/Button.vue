fb744b7cd80b44eea2a2893c07ef15694ee3f667
knipferrc <knipferrc@gmail.com>
2021-02-07 08:55:19 -0500

    fix types

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,11 +3,24 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Button',
+  props: {
+    type: {
+      type: String,
+      default: 'button',
+    },
+    onClick: {
+      type: Function,
+      required: true,
+    },
+  },
 });
 </script>
 
 <template>
-  <button class="bg-red-600 text-white px-4 py-2 rounded border-4 border-black">
+  <button
+    :type="type"
+    class="bg-red-600 text-white px-4 py-2 rounded border-4 border-black"
+  >
     <slot />
   </button>
 </template>
