d776c5979f6ca9e2523ea9af1602abc995ac5dde
knipferrc <knipferrc@gmail.com>
2021-02-07 09:15:54 -0500

    fix event

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -8,13 +8,16 @@ export default defineComponent({
       type: String as PropType<'button' | 'submit' | 'reset'>,
       default: 'button',
     },
+    onClick: {
+      type: Function,
+    },
   },
 });
 </script>
 
 <template>
   <button
-    @click="$emit('handleClick')"
+    @click="$emit('handle-click')"
     :type="type"
     class="bg-red-600 text-white px-4 py-2 rounded border-4 border-black"
   >
