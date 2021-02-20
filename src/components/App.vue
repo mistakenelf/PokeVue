481c998dd4dc8181a76a33ca11b284ee284d68fc
knipferrc <knipferrc@gmail.com>
2021-02-20 08:29:32 -0500

    move style to app component

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,3 +11,48 @@ export default defineComponent({
     <router-view />
   </component>
 </template>
+
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+html,
+body,
+#app,
+main {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #2a75bb;
+  font-family: 'Press Start 2P', cursive;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+  padding: 0;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+pre {
+  white-space: pre-line;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+  display: block;
+}
+</style>
