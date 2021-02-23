b619a45a7355208ead47c3e4d2bdc99a18cd4358
knipferrc <knipferrc@gmail.com>
2021-02-23 08:25:54 -0500

    use typescript for props

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,19 +1,19 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
 
 export default defineComponent({
   name: 'Spinner',
   props: {
     width: {
-      type: Number,
+      type: Number as PropType<number>,
       default: 30,
     },
     height: {
-      type: Number,
+      type: Number as PropType<number>,
       default: 30,
     },
     isOverlay: {
-      type: Boolean,
+      type: Boolean as PropType<boolean>,
       default: false,
     },
   },
